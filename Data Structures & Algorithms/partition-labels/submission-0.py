e20e45c93646f1978d481ecfe4ade47cfcb7a922
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-03-31 22:00:25 -0700

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        res = []
+        s_counter = Counter(s)
+        l = 0
+        current_set = set()
+        for r in range(len(s)):
+            current_char = s[r]
+            current_set.add(current_char)
+            s_counter[current_char] -= 1
+            if s_counter[current_char] == 0:
+                current_set.remove(current_char)
+            if not current_set:
+                res.append(r - l + 1)
+                l = r + 1
+                current_set = set()
+        return res
