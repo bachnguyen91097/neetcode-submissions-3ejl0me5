dbf1d78167c60c5681752958395db756471fe591
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-04-06 21:46:53 -0700

    Add: populating-next-right-pointers-in-each-node - submission-1

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-1.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        leftMost = root
+        while leftMost.left:
+            curr = leftMost
+            while curr:
+                curr.left.next = curr.right # perfect binary tree
+                if curr.next:
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            leftMost = leftMost.left
+        return root
