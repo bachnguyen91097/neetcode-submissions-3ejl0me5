8a579ddc09f72f212c0303a40c57c0b661a04ff8
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-04-07 22:28:34 -0700

    Add: parallel-courses - submission-1

---
+++ Data Structures & Algorithms/parallel-courses/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for relation in relations:
+            prevCourse, nextCourse = relation
+            adj[nextCourse].append(prevCourse)
+        visiting = set()
+        visited = set()
+        res = []
+        def dfs(course, adj, visiting, visited):
+            if course in visiting:
+                return False
+            if course in visited:
+                return True
+            visiting.add(course)
+            current_semester = []
+            for neighbor in adj[course]:
+                if not dfs(neighbor, adj, visiting, visited):
+                    return False
+                current_semester.append(neighbor)
+            visiting.remove(course)
+            visited.add(course)
+            if current_semester:
+                res.append(current_semester)  
+            return True
+        for course in range(1, n+1):
+            if not dfs(course, adj, visiting, visited):
+                return -1
+        return len(res) + 1
+                
+        
