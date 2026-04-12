a4fc728877b7b43c3d36cd9058d25440ebd75864
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-04-12 10:38:38 -0700

    Add: single-element-in-a-sorted-array - submission-5

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if m % 2 != 0:
+                m -= 1
+            if nums[m] != nums[m+1] and nums[m] != nums[m-1]:
+                return nums[m]
+            if nums[m] == nums[m+1]:
+                l = m + 2
+            else:
+                r = m - 1
+        return nums[l]
