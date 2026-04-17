8c2bef263a3a67aa1dd15ca70e86c7ed6de56b27
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-04-16 22:04:28 -0700

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cols = set()
+        diag = set()
+        antiDiag = set()
+
+        def helper(r):
+            if r == n:
+                return 1
+            total = 0
+            for c in range(n):
+                if c in cols or (r-c) in diag or (r + c) in antiDiag:
+                    continue
+
+                cols.add(c)
+                diag.add(r-c)
+                antiDiag.add(r+c)
+
+                total += helper(r+1)
+
+                cols.remove(c)
+                diag.remove(r-c)
+                antiDiag.remove(r+c)
+                
+            return total
+        return helper(0)
+        
