873f1286561a7258974a4db86431ed9d07aa1d23
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-06-27 09:36:05 -0700

    Add: meeting-rooms-iii - submission-0

---
+++ Data Structures & Algorithms/meeting-rooms-iii/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        # map to keep track room and its according number of time used
+        room_to_count_map = {}
+        for i in range(n):
+            room_to_count_map[i] = 0
+        
+        # sorted based on start time
+        sorted_meetings = sorted(meetings, key=lambda meeting: meeting[0])
+        # meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]
+
+        free_rooms = [i for i in range(n)]
+        busy_rooms = []
+
+        for meeting in sorted_meetings:
+            current_start, current_end = meeting
+            duration = current_end - current_start
+            while busy_rooms and busy_rooms[0][0] <= current_start:
+                end_time, room_number = heapq.heappop(busy_rooms)
+                heapq.heappush(free_rooms, room_number)
+            
+            if free_rooms:
+                room_number = heapq.heappop(free_rooms)
+                finish_time = current_end
+                room_to_count_map[room_number] += 1
+            else:
+                earliest_end, room_number = heapq.heappop(busy_rooms)
+                finish_time = earliest_end + duration
+                room_to_count_map[room_number] += 1
+            
+            
+            heapq.heappush(busy_rooms, (finish_time, room_number))
+        
+        current_max = float("-inf")
+        res = -1
+        for room_number, time_used in room_to_count_map.items():
+            if time_used > current_max:
+                res = room_number
+                current_max = time_used
+        return res 
+
+
+
