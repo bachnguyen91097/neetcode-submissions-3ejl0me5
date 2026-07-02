7caa16f3691e14cf1a01a8b2a3629fb66e2ad226
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2026-07-01 22:06:44 -0700

    Add: design-file-system - submission-1

---
+++ Data Structures & Algorithms/design-file-system/submission-1.py
@@ -0,0 +1,30 @@
+class FileSystem:
+
+    def __init__(self):
+        self.paths = set()
+        self.path_to_value = {}
+
+    def createPath(self, path: str, value: int) -> bool:
+        if path in self.path_to_value:
+            return False
+        path_list = path.split('/')[1:]
+        n = len(path_list)
+        if n == 1:
+            self.paths.add(path_list[-1])
+            self.path_to_value[path] = value
+        else:
+            for element in path_list[:n-1]:
+                if element not in self.paths:
+                    return False
+            self.paths.add(path_list[-1])
+            self.path_to_value[path] = value
+        return True    
+
+    def get(self, path: str) -> int:
+        return self.path_to_value.get(path, -1)
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
